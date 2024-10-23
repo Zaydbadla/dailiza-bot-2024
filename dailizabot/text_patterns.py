438adfc95efa0b0f1b05def1c36a211a5fa705c5
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-10-23 10:22:52 +0200

    Added Warum ... ... ... ... pattern

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -14,6 +14,10 @@ psychobabble = [
     "Bist du sicher, dass du {0} brauchst?"]],
     
     [r"Bist du ([^\?]*)", 
-     ["Du bist selber {0}.", "Du solltest überlegen ob du nicht {0} bist.", "Danke, ich wusste, dass ich {0} bin."]]
+     ["Du bist selber {0}.", "Du solltest überlegen ob du nicht {0} bist.", "Danke, ich wusste, dass ich {0} bin."]],
+    
+    [r"Warum (.*?) (.*?) (.*?) (.*)?",["Ich weiß nicht warum {1} {2} {3} {0}.", 
+                                    "Ich sollte dich fragen warum {1} {2} {3} {0}.",
+                                    "Ich glaube nicht dass {1} {2} {3} {0}."]]
 
 ]
