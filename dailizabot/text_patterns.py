e969448afb6c6d21710b6adf43daf8277782a9de
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-10-27 16:48:21 +0100

    fixed small mistake with question marks

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -9,14 +9,14 @@ psychobabble = [
     "Ich kann nicht klagen. Was ist mit dir?"]],
 
     [r"Ich brauche (.*)",
-    ["Warum brauchst du {0}?",
+    ["Warum brauchst du {0}",
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
     
-    [r"Bist du ([^\?]*)", 
+    [r"Bist du ([^\?]*)?", 
      ["Du bist selber {0}.", "Du solltest überlegen ob du nicht {0} bist.", "Danke, ich wusste, dass ich {0} bin."]],
     
-    [r"Warum (.*?) (.*?) (.*?) (.*)?",["Ich weiß nicht warum {1} {2} {3} {0}.", 
+    [r"Warum (.*?) (.*?) (.*?) (\w+)?",["Ich weiß nicht warum {1} {2} {3} {0}.", 
                                     "Ich sollte dich fragen warum {1} {2} {3} {0}.",
                                     "Ich glaube nicht dass {1} {2} {3} {0}."]],
     
